a03c5149bf429e8e970e8ec5853208b0ea2c226c
Kauah <menezeskauah@gmail.com>
2024-08-27 18:06:36 -0300

    cria dtos user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
+import { ReturnUserDto } from './dto/returnUser.dto';
 import { UserEntity } from './entities/user.entity';
 
 @Controller('user')
@@ -8,13 +9,16 @@ export class UserController {
   
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post('create-user')
   async createUser(@Body() createUser: CreateUserDto) {
     return this.userService.createUser(createUser);
   };
 
   @Get()
-  async getAllUser() {
-    return this.userService.getAllUsers();
-  }
+  async getAllUser(): Promise<ReturnUserDto[]> {
+    return (await this.userService.getAllUsers()).map(
+      (UserEntity) => new ReturnUserDto(UserEntity),
+    );
+  };
 };
