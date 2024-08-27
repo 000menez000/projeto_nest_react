4543b160acee75b847b753c70bf8d5c7407dfdf4
Kauah <menezeskauah@gmail.com>
2024-08-27 13:39:01 -0300

    cria rota em city pora buscar cidades por id do state

---
+++ src/city/city.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CityEntity } from 'src/city/entities/city.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CityService {
+
+    constructor(
+        @InjectRepository(CityEntity)
+        private readonly cityRepository: Repository<CityEntity>
+    ) {};
+
+    async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
+        return this.cityRepository.find({
+            where: {
+                stateId,
+            }
+        });
+    };
+}
